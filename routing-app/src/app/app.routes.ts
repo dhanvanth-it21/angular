4169a847f9045424f01cf8cb7561c54bf007638a
Dhanvanth Sathyamangalam Balakrishnan <dhanvanth.balakrishnan@int.trustrace.com>
2025-01-29 12:21:24 +0530

    routing

--- routing-app/src/app/app.routes.ts
+++ routing-app/src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { first } from 'rxjs';
+import { ChildAComponent } from './child-a/child-a.component';
 
-export const routes: Routes = [];
+
+const lazyLoading = () => import('./child-b/child-b.component').then(c => c.ChildBComponent);
+
+export const routes: Routes = [
+    {
+        path: 'first',
+        title: 'First Component',
+        component: FirstComponent,
+        children: [
+            {
+                path: 'child-a',
+                title: 'child-B',
+                component: ChildAComponent,
+            },
+            {
+                path: 'child-b',
+                title: 'Child B',
+                loadComponent: lazyLoading,
+            },
+        ]
+    },
+    {
+        path: 'second',
+        title: 'Second Component',
+        component: SecondComponent,
+    },
+    {
+        path: '',
+        redirectTo: '/first',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: FirstComponent,
+    }
+];
+
+
